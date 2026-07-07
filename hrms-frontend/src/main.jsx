5520db9e04cf20cc9732c0df19b1264dc37feb8f
Ali-Balwa77 <akbarabbasbalwa@gmail.com>
2026-07-07 18:19:00 +0530

    updated code

--- hrms-frontend/src/main.jsx
+++ hrms-frontend/src/main.jsx
@@ -10,7 +10,7 @@ import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'datatables.net-dt/css/dataTables.dataTables.min.css';
 
-const basename = '/';
+const basename = import.meta.env.VITE_APP_BASENAME;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
